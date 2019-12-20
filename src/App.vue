67e0b4d1750b55050261c48f0cfef0abb4b6051a
José <jolguin@devartis.com>
2019-12-20 03:12:01 -0300

    fst -m

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hw-pricing">
+    <div class="hw-logo">
+      <!-- <img src="" alt="hw-logo"> -->
+    </div>
+    <div class="hw-main-wrapper">
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      algo: 'asdas dsa d'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+@import './assets/less/main.less';
 </style>
